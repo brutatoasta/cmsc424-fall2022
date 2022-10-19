6f85a234b788cb203ac428ceb8bce39ea9adb0ee
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-10-19 00:38:42 -0400

    add indent to schema file for folding add curl to makefile for testing add delete() to rest.py add close to get()

--- Assignment-1/schema.sql
+++ Assignment-1/schema.sql
@@ -14,14 +14,14 @@ CREATE TABLE PostTypes (
 );
 
 insert into PostTypes values 
-(1, 'Question'),
-(2, 'Answer'),
-(3, 'Orphaned tag wiki'),
-(4, 'Tag wiki excerpt'),
-(5, 'Tag wiki'),
-(6, 'Moderator nomination'),
-(7, 'Wiki placeholder'),
-(8, 'Privilege wiki');
+	(1, 'Question'),
+	(2, 'Answer'),
+	(3, 'Orphaned tag wiki'),
+	(4, 'Tag wiki excerpt'),
+	(5, 'Tag wiki'),
+	(6, 'Moderator nomination'),
+	(7, 'Wiki placeholder'),
+	(8, 'Privilege wiki');
 
 CREATE TABLE Posts (
 	Id integer NOT NULL PRIMARY KEY,
