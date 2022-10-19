c934fb273bd82a705028629efc3ad7b9133430fd
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-10-19 01:38:59 -0400

    add connectDB() add dbmd halfway

--- Assignment-3/MetaData.java
+++ Assignment-3/MetaData.java
@@ -15,10 +15,62 @@ public class MetaData
 		}
 		return "Other";
 	}
+	public static Connection connectDB(String databaseName){
+		Connection conn = null;
+		 // Load the PostgreSQL JDBC Driver
+		 System.out.println("-------- PostgreSQL " + "JDBC Connection Testing ------------");
+		 try {
+				 Class.forName("org.postgresql.Driver");
+		 } catch (ClassNotFoundException e) {
+				 System.out.println("Where is your PostgreSQL JDBC Driver? " + "Include in your library path!");
+				 e.printStackTrace();
+				 return null;
+		 }
+		 System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		 // Set up the conn
+		
+		 try {
+				String url = String.format("jdbc:postgresql://localhost:5432/%s","root", "root", databaseName);
+				 conn = DriverManager.getConnection(url);
+		 } catch (SQLException e) {
+				 System.out.println("Connection Failed! Check output console");
+				 e.printStackTrace();
+				 return null;
+		 }
+
+		 if (conn != null) {
+				 System.out.println("You made it, take control your database now!");
+				 return conn;
+		 } else {
+				 System.out.println("Failed to make conn!");
+				 return null;
+		 }
+	}
 	public static void executeMetadata(String databaseName) {
 		/************* 
-		 * Add you code to connect to the database and print out the metadta for the database databaseName. 
+		 * Add you code to connect to the database and print out the metadata for the database databaseName. 
 		 ************/
+		Connection conn = connectDB(databaseName);
+		DatabaseMetaData dbmd =null;
+		try{
+			dbmd = conn.getMetaData();
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		try{ 
+			ResultSet resultSet = dbmd.getTables(null, null, null, new String[]{"TABLE"});
+			while(resultSet.next()) { 
+			  String tableName = resultSet.getString("TABLE_NAME"); 
+			  String remarks = resultSet.getString("REMARKS"); 
+			}
+		  }
+		  catch (SQLException e){
+			e.printStackTrace();
+		}
+
+
 	}
 
 	public static void main(String[] argv) {
